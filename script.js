2d16192ff2fab6bcb5f36e3974e8e25cb5aadc44
prsvas <prsvas@gmail.com>
2026-08-15 00:46:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const printBtn = document.getElementById("printBtn");
+
+  if (printBtn) {
+    printBtn.addEventListener("click", () => {
+      window.print();
+    });
+  }
+});
